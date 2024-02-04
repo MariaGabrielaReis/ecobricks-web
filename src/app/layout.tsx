1d03126e2f7a8cea25779acee8184a8278fcd384
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-04 12:35:24 -0300

    feat: create and use navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
 import { Box, ThemeProvider } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-import type { Metadata } from "next";
 
+import { Navbar } from "@/components/Navbar";
 import theme from "@/theme";
+
+export const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ecobricks",
@@ -16,20 +21,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
-            <Box
-              component="main"
-              sx={{
-                flexGrow: 1,
-                bgcolor: "background.default",
-                mt: ["122px", "135px", "146px"],
-                p: 3,
-              }}
-            >
-              {children}
-            </Box>
+            <div>
+              <Navbar />
+              <Box
+                component="main"
+                sx={{
+                  flexGrow: 1,
+                  bgcolor: "background.default",
+                  mt: ["122px", "135px", "146px"],
+                  p: 3,
+                }}
+              >
+                {children}
+              </Box>
+            </div>
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
