52cf5f5cf08d92bbe55b558ca7d611294acdae0e
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-06 18:42:48 -0300

    feat: always redirect to /products

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => {
+    return [{ source: "/", destination: "/products", permanent: true }];
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "source.unsplash.com" },
